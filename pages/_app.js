d16d859f4b72bb7aa6d5a580635e63a576e081c2
lazim17 <lazimahamed2002@gmail.com>
2024-02-20 13:46:43 +0530

    First commit

---
+++ pages/_app.js
@@ -0,0 +1,38 @@
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { UseWalletProvider } from "use-wallet";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "@fontsource/space-grotesk";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Space Grotesk",
+    body: "Space Grotesk",
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {" "}
+      <ChakraProvider theme={theme}>
+        <UseWalletProvider
+          chainId={11155111}
+          connectors={{
+            walletconnect: {
+              rpcUrl:
+                "https://sepolia.infura.io/v3/c935c9936531469d9a345c7b4eae2302",
+            },
+          }}
+        >
+          <NavBar />
+          <Component {...pageProps} />
+          <Footer />{" "}
+        </UseWalletProvider>
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default MyApp;
